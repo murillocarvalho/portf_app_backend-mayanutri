e924eb15c7158dccdc5c0ac363c8cabe95bd1a61
Murillo Carvalho <murillohpcarvalho@gmail.com>
2026-08-22 22:28:18 -0300

    fix exception and code

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
-import os
 from supabase import create_client, Client
 from postgrest.exceptions import APIError
 from httpx import RequestError
